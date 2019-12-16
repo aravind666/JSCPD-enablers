aea8f3502ded56ff5c693569c2d716a8af69ac9d
Aravind HU <aravind.hu@philips.com>
2019-12-16 13:53:19 +0530

    Added proxy configuration to bat

--- jscpd-installer-for-windows.bat
+++ jscpd-installer-for-windows.bat
@@ -19,7 +19,7 @@ IF %NODE_VER% NEQ null (
 	IF NOT EXIST tmp/%NODE_EXEC% (
 		echo Node setup file does not exist. Downloading ...
 		cd ../bin
-		START /WAIT wget http://nodejs.org/dist/v0.8.11/%NODE_EXEC%
+		START /WAIT wget https://nodejs.org/dist/v0.8.11/%NODE_EXEC%
 		move %NODE_EXEC% %SETUP_DIR%/tmp
 	)
 	cd %SETUP_DIR%/tmp
@@ -30,6 +30,11 @@ IF %NODE_VER% NEQ null (
 )
 
 cd ../..
+echo SETUP PROXY for PIC network ...
+call npm config set http-proxy http://165.225.96.34:9480
+call npm config set https-proxy http://165.225.96.34:9480
+echo SETUP Node package manager to lookup for packages  ...
+call npm config set registry https://registry.npmjs.org/
 echo INSTALLING jscpd ...
 call npm install -g jscpd
 REM echo INSTALLING jscpd command line tool ......
